8351ce0cfbea557eeb68cfd109b7cf5bc905b41c
Alejandro Armagnac <a.armagnac.l@gmail.com>
2011-04-09 22:31:57 -0500

    README

---
+++ erss.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import urllib
+import urllib2
+import cookielib
+import emagister2rss
+
+def login(email, password):
+    cookie = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+    data = urllib.urlencode({"mail":email,"password":password})
+    url_s = "http://grupos.emagister.com/index/signin"
+
+    opener.open(url_s,data)
+    
+    return opener
+
+def EtoRss(opener):
+    url = "http://grupos.emagister.com"
+    urls = {'wall':url+"/userhome/#wall_all",
+            'groups':url+"/perfil/grupos",}
+
+    for url_ in urls:
+        s = opener.open(urls[url_])
+        getattr(emagister2rss,'rss_'+url_)(s.read(),opener)
+
+def run(email,password):
+    import time
+    opener = login(email,password)
+    while(1):
+        EtoRss(opener)
+        time.sleep(60)
--- server.py
+++ server.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
 import web
-
 
 class index:
     def GET(self):
-        return "Hello, World"
+        return "Nada por aquí"
 
 urls = (
     '/','index'
@@ -12,4 +12,17 @@ urls = (
 app = web.application(urls, globals())
 
 if __name__ == "__main__":
+    import os
+    import thread
+    import getpass
+    import erss
+    #look for static/feeds dir
+    if os.path.exists("static/feeds") == False:
+        os.mkdir("static")
+        os.mkdir("static/feeds")
+    #Enter email and password to login in grupos.emagister.com
+    email = raw_input("Enter your email: ")
+    password = getpass.getpass("Password: ")
+    thread.start_new_thread(erss.run,(email,password))
     app.run()
+
